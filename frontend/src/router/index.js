0d4c9ba6dad382c665d916b46ebee9669b478da0
xnm-sdksdk <n.mendonca@protonmail.com>
2024-03-16 17:33:07 +0000

    routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,157 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LandingPage from '@/views/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: '',
+      component: LandingPage
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      name: 'login',
+      component: loginView,
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/users',
+      name: '/users/{userId}',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/badges',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/badges/{badgeId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/posts',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/posts/{postsId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/posts/{postsId}/comments',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/posts/{postsId}/comments/{commentId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/posts/{postsId}/like',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/reports',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/reports',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/diary',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/diary/{entryId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/activities',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/activities/{activityId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/categories',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },{
+      path: '/categories/{categoryId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/objectives',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/objectives/{objectiveId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/resources',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '/resources/{resourceId}',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
+    {
+      path: '',
+      name: '',
+      component: '',
+      meta: { requiresAuth: true},
+    },
   ]
 })
 
