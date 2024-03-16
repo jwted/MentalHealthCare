71aa5797e1175d1851b9e9e520904eadfd524028
xnm-sdksdk <n.mendonca@protonmail.com>
2024-03-16 17:33:31 +0000

    store

---
+++ frontend/src/store/globalStore.js
@@ -0,0 +1,9 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useGlobalStore = defineStore('globalStore', () => {
+  state: () => ({
+  }),
+  getters: {},
+  actions: {},
+})
