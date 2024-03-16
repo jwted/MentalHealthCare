2accf915c9d323368e8b371f75245e0ebb0a0836
xnm-sdksdk <n.mendonca@protonmail.com>
2024-03-16 17:33:41 +0000

    visual changes landing page navbar

--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -1,9 +1,13 @@
 <template>
+    <Navbar></Navbar>
     <h1>Inner Peace</h1>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar/Navbar.vue';
 export default {
-
+components: {
+    Navbar
+}
 }
 </script>
