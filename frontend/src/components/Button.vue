2e68e4868eea8f9e5b0eaa81f6bfb5baa795c845
FredSilva21 <fredsilva43@gmail.com>
2024-03-29 11:37:53 +0000

    landing page content already done

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button>Start</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+button{
+    background-color: #ADDFAD;
+    color: #2E4242;
+    padding: 0.5rem 1rem;
+    border: none;
+    border-radius: 12px;
+    border:2px solid #2E4242;
+    font-size: 16pt;
+    cursor: pointer;
+    width: 100px;
+}
+</style>
--- frontend/src/views/LandingPage.vue
+++ frontend/src/views/LandingPage.vue
@@ -22,54 +22,150 @@
       <!--Carrossel que mostra de 3 em em 3 cards-->
       <h2>Our Courses</h2>
       <div class="cards">
-        <v-card
-        class="mx-auto card"
-        width="400"
-      >
-        <div class="d-flex align-center justify-space-between">
-          <img src="../assets/depression.svg" alt="" />
-          <v-card-title class="text-center">Depression</v-card-title>
-        </div>
+        <v-card class="mx-auto card" width="400">
+          <div class="d-flex align-center justify-space-between">
+            <img src="../assets/depression.svg" alt="" />
+            <v-card-title
+              class="text-center flex-grow-1 d-flex justify-center align-center"
+              >Depression</v-card-title
+            >
+          </div>
 
-        <v-card-text class="text-justify">
-          Welcome to a journey of self-improvement. Our platform is designed to
-          guide you through the process of personal growth, step by step.
-          Whether you're looking to enhance your skills, build confidence, or
-          achieve your goals, we're here to support you every step of the way.
-        </v-card-text>
-      </v-card>
-      <v-card
-        class="mx-auto card"
-        width="400"
-      >
-        <div class="d-flex align-center justify-space-between">
-          <img src="../assets/depression.svg" alt="" />
-          <v-card-title class="text-center">Depression</v-card-title>
-        </div>
+          <v-card-text class="text-justify">
+            Welcome to a journey of self-improvement. Our platform is designed
+            to guide you through the process of personal growth, step by step.
+            Whether you're looking to enhance your skills, build confidence, or
+            achieve your goals, we're here to support you every step of the way.
+          </v-card-text>
+        </v-card>
+        <v-card class="mx-auto card" width="400">
+          <div class="d-flex align-center justify-space-between">
+            <img src="../assets/depression.svg" alt="" />
+            <v-card-title
+              class="text-center flex-grow-1 d-flex justify-center align-center"
+              >Depression</v-card-title
+            >
+          </div>
 
-        <v-card-text class="text-justify">
-          Welcome to a journey of self-improvement. Our platform is designed to
-          guide you through the process of personal growth, step by step.
-          Whether you're looking to enhance your skills, build confidence, or
-          achieve your goals, we're here to support you every step of the way.
-        </v-card-text>
-      </v-card>
-      <v-card
-        class="mx-auto card"
-        width="400"
-      >
-        <div class="d-flex align-center justify-space-between">
-          <img src="../assets/depression.svg" alt="" />
-          <v-card-title class="text-center">Depression</v-card-title>
-        </div>
+          <v-card-text class="text-justify">
+            Welcome to a journey of self-improvement. Our platform is designed
+            to guide you through the process of personal growth, step by step.
+            Whether you're looking to enhance your skills, build confidence, or
+            achieve your goals, we're here to support you every step of the way.
+          </v-card-text>
+        </v-card>
+        <v-card class="mx-auto card" width="400">
+          <div class="d-flex align-center justify-space-between">
+            <img src="../assets/depression.svg" alt="" />
+            <v-card-title
+              class="text-center flex-grow-1 d-flex justify-center align-center"
+              >Depression</v-card-title
+            >
+          </div>
 
-        <v-card-text class="text-justify">
-          Welcome to a journey of self-improvement. Our platform is designed to
-          guide you through the process of personal growth, step by step.
-          Whether you're looking to enhance your skills, build confidence, or
-          achieve your goals, we're here to support you every step of the way.
-        </v-card-text>
-      </v-card>
+          <v-card-text class="text-justify">
+            Welcome to a journey of self-improvement. Our platform is designed
+            to guide you through the process of personal growth, step by step.
+            Whether you're looking to enhance your skills, build confidence, or
+            achieve your goals, we're here to support you every step of the way.
+          </v-card-text>
+        </v-card>
+      </div>
+    </div>
+    <div class="d-flex flex-column pd-1rem">
+      <h2>Your path to grow</h2>
+      <v-row>
+        <v-col class="bg"> asdasda</v-col>
+        <v-col>asasd</v-col>
+      </v-row>
+      <v-row>
+        <v-col>asasd</v-col>
+        <v-col>asasd</v-col>
+      </v-row>
+      <v-row>
+        <v-col>asasd</v-col>
+        <v-col>asasd</v-col>
+      </v-row>
+    </div>
+
+    <div>
+      <h2>How InnerPeace changed lives</h2>
+      <div class="cards">
+        <v-card class="mx-auto card" width="400">
+          <div class="d-flex align-center justify-space-between">
+            <img src="../assets/depression.svg" alt="" />
+            <v-card-title
+              class="text-center flex-grow-1 d-flex justify-center align-center"
+              >Depression</v-card-title
+            >
+          </div>
+
+          <v-card-text class="text-justify">
+            Welcome to a journey of self-improvement. Our platform is designed
+            to guide you through the process of personal growth, step by step.
+            Whether you're looking to enhance your skills, build confidence, or
+            achieve your goals, we're here to support you every step of the way.
+          </v-card-text>
+        </v-card>
+        <v-card class="mx-auto card" width="400">
+          <div class="d-flex align-center justify-space-between">
+            <img src="../assets/depression.svg" alt="" />
+            <v-card-title
+              class="text-center flex-grow-1 d-flex justify-center align-center"
+              >Depression</v-card-title
+            >
+          </div>
+
+          <v-card-text class="text-justify">
+            Welcome to a journey of self-improvement. Our platform is designed
+            to guide you through the process of personal growth, step by step.
+            Whether you're looking to enhance your skills, build confidence, or
+            achieve your goals, we're here to support you every step of the way.
+          </v-card-text>
+        </v-card>
+      </div>
+      <div class="cards">
+        <v-card class="mx-auto card" width="400">
+          <div class="d-flex align-center justify-space-between">
+            <img src="../assets/depression.svg" alt="" />
+            <v-card-title
+              class="text-center flex-grow-1 d-flex justify-center align-center"
+              >Depression</v-card-title
+            >
+          </div>
+
+          <v-card-text class="text-justify">
+            Welcome to a journey of self-improvement. Our platform is designed
+            to guide you through the process of personal growth, step by step.
+            Whether you're looking to enhance your skills, build confidence, or
+            achieve your goals, we're here to support you every step of the way.
+          </v-card-text>
+        </v-card>
+        <v-card class="mx-auto card" width="400">
+          <div class="d-flex align-center justify-space-between">
+            <img src="../assets/depression.svg" alt="" />
+            <v-card-title
+              class="text-center flex-grow-1 d-flex justify-center align-center"
+              >Depression</v-card-title
+            >
+          </div>
+
+          <v-card-text class="text-justify">
+            Welcome to a journey of self-improvement. Our platform is designed
+            to guide you through the process of personal growth, step by step.
+            Whether you're looking to enhance your skills, build confidence, or
+            achieve your goals, we're here to support you every step of the way.
+          </v-card-text>
+        </v-card>
+      </div>
+    </div>
+    <div class="container-1">
+      <div>
+        <img src="../assets/road.svg" alt="Stairs Image" class="image" />
+      </div>
+      <div class="noImage">
+        <h1 class="title">Start your road to hapiness today</h1>
+        <button class="button">Start</button>
       </div>
     </div>
   </main>
@@ -138,11 +234,13 @@ export default {
 h2 {
   color: #2e4242;
   font-size: 28pt;
+  margin-bottom: 2rem;
 }
 
 .cards {
   display: flex;
   justify-content: space-between;
-  width: 80%;
+  width: 90%;
+  margin-top: 2rem;
 }
 </style>
