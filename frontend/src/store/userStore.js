833bb9243a18f1ea7c818e4df848313be41abc85
FredSilva21 <fredsilva43@gmail.com>
2024-04-08 16:09:37 +0100

    userStore

--- frontend/src/store/userStore.js
+++ frontend/src/store/userStore.js
@@ -3,18 +3,19 @@ import { defineStore } from "pinia";
 
 const url = "https//innerpeace.com";
 export const userStore = defineStore("user", {
-  state: () => ({ users: [] }),
+  state: () => ({ users: [], loggedInUser: null }),
   getters: {
-    doubleCount: (state) => state.count * 2,
+    getLoggedInUser: (state) => state.loggedInUser,
+    getUsers: (state) => state.users,
   },
   actions: {
-    async fetchUser() {
+    async fetchUsers() {
       const response = await fetch(`${url}/users`);
       const data = await response.json();
       this.users = data;
     },
 
-    async createUser(user) {
+    async register(user) {
       const response = await fetch(`${url}/users`, {
         method: "POST",
         body: JSON.stringify(user),
@@ -32,6 +33,36 @@ export const userStore = defineStore("user", {
       });
       const data = await response.json();
       this.users.push(data);
+    },
+
+    async logout() {
+      this.loggedInUser = null;
+    },
+
+    async updateUser(user) {
+      const response = await fetch(`${url}/users/${user.id}`, {
+        method: "PUT",
+        body: JSON.stringify(user),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      this.users = this.users.map((us) => (us.id === user.id ? data : u));
+    },
+
+    async deleteUser(user) {
+      try {
+        const response = await fetch(`${url}/users/${user.id}`, {
+          method: "DELETE",
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error deleting user: ${response.statusText}`);
+        }
+
+        this.users = this.users.filter((us) => us.id !== user.id);
+      } catch (error) {
+        console.error("Error deleting user:", error);
+      }
     },
   },
 });
