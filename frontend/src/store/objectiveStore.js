db7490cffff5098194afd0416e04eb499127f674
FredSilva21 <fredsilva43@gmail.com>
2024-04-10 13:26:05 +0100

    objective and post store

---
+++ frontend/src/store/objectiveStore.js
@@ -0,0 +1,62 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+const url = "https//innerpeace.com";
+export const objectiveStore = defineStore("objective", {
+  state: () => ({ objectives: [], objective: {}}),
+  getters: {
+    getObjectives: (state) => state.objectives,
+    getObjective: (state) => state.objective,
+  },
+  actions: {
+    async fetchObjectives() {
+      const response = await fetch(`${url}/objectives`);
+      const data = await response.json();
+      this.objectives = data;
+    },
+
+    async addObjective(objective) {
+      const response = await fetch(`${url}/objectives`, {
+        method: "POST",
+        body: JSON.stringify(objective),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      this.objectives.push(data);
+    },
+
+    async updateObjective(objective) {
+      const response = await fetch(`${url}/objectives/${objective.id}`, {
+        method: "PUT",
+        body: JSON.stringify(objective),
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+      this.objectives = this.objectives.map((o) => (o.id === objective.id ? data : o));
+    },
+
+    async deleteObjective(objective) {
+      try {
+        const response = await fetch(`${url}/objectives/${objective.id}`, {
+          method: "DELETE",
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error deleting objective: ${response.statusText}`);
+        }
+
+        this.objectives = this.objectives.filter((o) => o.id !== objective.id);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+
+    async getObjective(id) {
+      const response = await fetch(`${url}/objectives/${id}`);
+      const data = await response.json();
+      this.objective = data;
+    }
+  },
+
+  async 
+});
