c6b004c424c3f653011950789ca341bfced4f9b4
FredSilva21 <fredsilva43@gmail.com>
2024-06-02 15:05:15 +0100

    add post done

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -1,41 +1,85 @@
 <template>
-        <Navbar></Navbar>
-        <main v-for="post in getPosts" :key="post.id">
-            <PostContainer :post="post"></PostContainer>
-        </main>
-        <Footer></Footer>
+  <Navbar></Navbar>
+  <v-container>
+    <v-row cols="10" class="d-flex align-center justify-space-between">
+      <v-col cols="5" class="d-flex justify-start">
+        <h2>Community</h2>
+      </v-col>
+      <v-col cols="5" class="d-flex justify-end">
+        <Button :text="'Back'" @click="router.go(-1)" class="w-25"></Button>
+      </v-col>
+    </v-row>
+  </v-container>
+  <main class="d-flex flex-column">
+    <v-container>
+      <v-row cols="12" class="d-flex align-center justify-space-between">
+        <v-col cols="12">
+          <textarea
+            id="new-post-input"
+            class="w-100 area"
+            rows="10"
+            placeholder="Share your thoughts..."
+            v-model="textPost"
+          ></textarea>
+          <Button :text="'Post'" @click="addPost"></Button>
+        </v-col>
+      </v-row>
+      <v-row v-for="post in getPosts" :key="post.id" cols="10">
+        <v-col>
+          <PostContainer :post="post"></PostContainer>
+        </v-col>
+      </v-row>
+    </v-container>
+  </main>
+  <Footer></Footer>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue'
-import PostContainer from '../components/PostContainer.vue';
-import Footer from '../components/Footer.vue'
-import {postStore} from '../store/postStore'
+import Navbar from "../components/Navbar.vue";
+import PostContainer from "../components/PostContainer.vue";
+import Button from "../components/Button.vue";
+import Footer from "../components/Footer.vue";
+import { postStore } from "../store/postStore";
 export default {
-    components: {
-        Navbar,
-        PostContainer,
-        Footer
-    },
+  components: {
+    Navbar,
+    PostContainer,
+    Footer,
+    Button,
+  },
 
-    data() {
-        return {
-            postStore: postStore()
-        }
-    },
+  data() {
+    return {
+      postStore: postStore(),
+      textPost: "",
+    };
+  },
 
-    created () {
-        this.postStore.getPosts();
-    },
+  created() {
+    this.postStore.getPosts();
+  },
 
-    computed: {
-        getPosts() {
-            return this.postStore.getAllPosts
-        }
-    }
-}
+  computed: {
+    getPosts() {
+      return this.postStore.getAllPosts;
+    },
+  },
+
+  methods: {
+    addPost() {
+      this.postStore.addPost(this.textPost);
+      this.textPost = "";
+    },
+  },
+};
 </script>
 
 <style>
-
+.area {
+  border: #2e4242 solid 2px;
+  border-radius: 12px;
+  padding: 10px;
+  margin-bottom: 10px;
+  font-size: 16pt;
+}
 </style>
